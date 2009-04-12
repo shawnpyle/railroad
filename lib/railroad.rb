bf78b49884faf0aab9736de5153933f394df6609
Bryan Larsen <bryan@larsen.st>
2009-04-12 11:28:29 -0400

    Bump version.

--- lib/railroad.rb
+++ lib/railroad.rb
@@ -1,6 +1,6 @@
 APP_NAME       = "railroad"
 APP_HUMAN_NAME = "RailRoad"
-APP_VERSION    = [0,7,6]
+APP_VERSION    = [0,7,7]
 COPYRIGHT      = "Copyright (C) 2007-2008 Javier Smaldone"
 
 require 'railroad/options_struct'
