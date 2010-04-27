fcbcd068515076ea635d3c5b7c626e10fa2db6ae
Steve Smith <github@scsworld.co.uk>
2010-04-27 11:55:40 +0100

    Move tasks to lib/tasks for Rails 3. Change PLATFORM to RUBY_PLATFORM for Ruby 1.9

--- lib/railroad/app_diagram.rb
+++ lib/railroad/app_diagram.rb
@@ -57,7 +57,7 @@ class AppDiagram
   # Prevents Rails application from writing to STDOUT
   def disable_stdout
     @old_stdout = STDOUT.dup
-    STDOUT.reopen(PLATFORM =~ /mswin/ ? "NUL" : "/dev/null")
+    STDOUT.reopen(RUBY_PLATFORM || PLATFORM =~ /mswin/ ? "NUL" : "/dev/null")
   end
 
   # Restore STDOUT  
--- lib/tasks/diagrams.rake
+++ lib/tasks/diagrams.rake
@@ -1,4 +1,4 @@
-$:.unshift File.join(File.dirname(__FILE__), '../..')
+$:.unshift File.join(File.dirname(__FILE__), '..')
 require 'railroad'
 
 namespace :doc do
